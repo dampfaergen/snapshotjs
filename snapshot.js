e8338d9ad57dd370ff7fecf023643e2c7a8e9232
Søren Louv <sorenlouv@gmail.com>
2012-11-11 20:56:01 +0100

    outputting whoami

--- snapshot.js
+++ snapshot.js
@@ -35,30 +35,38 @@ http.createServer(function (req, res) {
   var urlFragment = urlParsed.query.url_fragment;
   
 
-  // deploy new version
-  if(urlParsed.query.deploy){
-    console.log("Deploying app");
-    var snapshot = exec("git pull origin master", function(error, stdOut, stdError){
-      console.log(stdOut);
-      res.write("Deployed!");
-      res.write(stdError);
-      res.write(error);
-      res.write(stdOut);
-      res.end();
-    });
-    
-  // take snapshot
-  }else if(urlParsed.query.url_fragment){
-    console.log("Taking screenshot");
-    takeSnapshot(urlFragment, function(output){
-      res.write(output);
-      res.end();
-    });
 
-  // do nothing
-  }else{
-    console.log("Doing nothing with: " + urlParsed.path);
-    res.end("Doing nothing with: " + urlParsed.path);
-  }
+  exec("whoami", function(error, stdOut, stdError){
+    console.log(stdOut);
+    res.write("Deployed!");
+    res.write(stdError);
+    res.write(stdOut);
+    res.end();
+  });
+
+  // // deploy new version
+  // if(urlParsed.query.deploy){
+  //   console.log("Deploying app");
+  //   var snapshot = exec("git pull origin master", function(error, stdOut, stdError){
+  //     console.log(stdOut);
+  //     res.write("Deployed!");
+  //     res.write(stdError);
+  //     res.write(stdOut);
+  //     res.end();
+  //   });
+    
+  // // take snapshot
+  // }else if(urlParsed.query.url_fragment){
+  //   console.log("Taking screenshot");
+  //   takeSnapshot(urlFragment, function(output){
+  //     res.write(output);
+  //     res.end();
+  //   });
+
+  // // do nothing
+  // }else{
+  //   console.log("Doing nothing with: " + urlParsed.path);
+  //   res.end("Doing nothing with: " + urlParsed.path);
+  // }
 
 }).listen(9090, '127.0.0.1'); //.listen(9090, '178.79.137.106');
